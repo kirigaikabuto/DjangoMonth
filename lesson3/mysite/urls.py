6b799a7e2204f60bfd4e866ccf1d402b4ea0c8d5
Yerassyl Tleugazy <ytleugazy@dar.kz>
2021-01-28 19:24:06 +0600

    sdsd

--- lesson3/mysite/urls.py
+++ lesson3/mysite/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import homePage
+from .views import homePage, formAction
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", homePage, name="home_page"),
+    path("formAction/", formAction, name="form_action"),
 ]
--- lesson3/mysite/views.py
+++ lesson3/mysite/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+import random
 
 
 def homePage(request):
     name = "Yerassyl"
     lastName = "Tleugazy"
+    number = str(random.randint(0, 30))
     d = {
         "first_name": name,
         "last_name": lastName,
+        "number": number,
     }
     return render(request, "frontend/index.html", context=d)
+
+
+def formAction(request):
+    username = request.POST["username"]
+    print("data from form", username)
+    return HttpResponse("Hello " + username)

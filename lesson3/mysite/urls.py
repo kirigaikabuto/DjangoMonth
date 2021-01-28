38eada05e60baaddebf5d4ae89c15f00a4631850
Yerassyl Tleugazy <ytleugazy@dar.kz>
2021-01-28 20:26:48 +0600

    sdsd

--- lesson3/mysite/urls.py
+++ lesson3/mysite/urls.py
@@ -4,8 +4,12 @@ from .views import homePage, loginAction, loginPage, registerPage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path("", homePage, name="home_page"),
+
     path("loginAction/", loginAction, name="login_action"),
     path("loginPage/", loginPage, name="login_page"),
+
     path("registerPage/", registerPage, name="register_page"),
+    # registerAction
 ]

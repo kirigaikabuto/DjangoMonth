26d7829d11a5edcacdbb878f6067753a417dfbea
kirigaikabuto <tleugazy98@gmail.com>
2021-02-04 18:45:15 +0600

    sdsd

---
+++ lesson4/mysite/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from .views import homePage, loginAction, loginPage, registerPage
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path("", homePage, name="home_page"),
+
+    path("loginAction/", loginAction, name="login_action"),
+    path("loginPage/", loginPage, name="login_page"),
+
+    path("registerPage/", registerPage, name="register_page"),
+    #registerAction/
+]
---
+++ lesson4/mysite/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render, HttpResponse
+import random
+
+users = [
+    {
+        "username": "yerassyl",
+        "password": "passanya99",
+        "first_name": "yerassyl",
+        "last_name": "tleugazy",
+        "age": 22,
+    },
+    {
+        "username": "kirito",
+        "password": "732110",
+        "first_name": "kirigai",
+        "last_name": "kabuto",
+        "age": 16
+    },
+]
+
+
+def homePage(request):
+    name = "Yerassyl"
+    lastName = "Tleugazy"
+    number = str(random.randint(0, 30))
+    d = {
+        "first_name": name,
+        "last_name": lastName,
+        "number": number,
+        "users": users,
+    }
+    return render(request, "frontend/index.html", context=d)
+
+
+def loginAction(request):
+    username = request.POST["username"]
+    password = request.POST['pass']
+    if len(username) == 0:
+        return HttpResponse("Please write your username")
+    if len(password) == 0:
+        return HttpResponse("Please write your password")
+    isExist = False
+    d = {}
+    for i in users:
+        if i["username"] == username and i["password"] == password:
+            isExist = True
+            d = i
+            break
+
+    message = "Incorrect username and password"
+    if isExist:
+        return render(request, "frontend/profile.html", context=d)
+
+    return HttpResponse(message)
+
+
+def loginPage(request):
+    return render(request, "frontend/login.html")
+
+
+def registerPage(request):
+    return render(request, "frontend/register.html")
+
+#view for registerAction

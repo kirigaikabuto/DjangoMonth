0089dcb5a06495a901d95297d000b2c9e2020d7d
kirigaikabuto <tleugazy98@gmail.com>
2021-02-11 19:32:17 +0600

    sdsddsd

--- lesson7/products/views.py
+++ lesson7/products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+def list(request):
+    products = Product.objects.all()
+    for i in products:
+        print(i.name, i.price)
+    d = {
+        "products": products,
+    }
+    return render(request, "products/list.html", context=d)

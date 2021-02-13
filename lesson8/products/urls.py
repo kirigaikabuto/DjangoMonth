6711f60beb992043fec8f07d97ee3a7dbd4e52d8
kirigaikabuto <tleugazy98@gmail.com>
2021-02-13 13:33:11 +0600

    sdsd

--- lesson8/products/urls.py
+++ lesson8/products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("list/", list, name="product_list")
+    path("list/", list, name="product_list"),
+    path("get/<int:id>/", get, name="product_get"),
 ]
--- lesson8/products/views.py
+++ lesson8/products/views.py
@@ -3,10 +3,16 @@ from .models import Product
 
 
 def list(request):
-    products = Product.objects.all()
-    for i in products:
-        print(i.name, i.price)
+    products = Product.objects.all()  # [Product1,Product2,Product]
     d = {
         "products": products,
     }
     return render(request, "products/list.html", context=d)
+
+
+def get(request, id):
+    product = Product.objects.get(pk=id)  # Product1 or Product2
+    d = {
+        "product": product,
+    }
+    return render(request, "products/get.html", context=d)

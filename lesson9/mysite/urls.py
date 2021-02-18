4b30d2ff4d68f63804d9eaa39289a1bbd0c7e23e
kirigaikabuto <tleugazy98@gmail.com>
2021-02-18 20:17:27 +0600

    sdsd

--- lesson9/mysite/urls.py
+++ lesson9/mysite/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("products/", include("products.urls")),
     path("", include("main.urls")),
+    path("users/", include("users.urls")),
 ]
